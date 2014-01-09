0264bb7bf703cd1a28ec800edb7741ace8a00ada
Andrew Milsted <a.j.milsted@soton.ac.uk>
2014-01-09 16:06:46 +0000

    Added Changes to style etc

--- htdocs/app/org.php
+++ htdocs/app/org.php
@@ -8,8 +8,9 @@ class org {
 		$idtype = $f3->get( "PARAMS.type" );
 		$idtype = preg_replace( '/[^-a-z0-9]/i','',$idtype );
 		$id = $f3->get( "PARAMS.id" );
-		$suffix = pathinfo($id, PATHINFO_EXTENSION);
-		$id = preg_replace( '/[^-a-z0-9]/i','', pathinfo($id, PATHINFO_FILENAME) );
+		$datset = $f3->get( "PARAMS.dataset" );
+		$suffix = pathinfo($datset, PATHINFO_EXTENSION);
+		$datset = preg_replace( '/[^-a-z0-9]/i','', pathinfo($datset, PATHINFO_FILENAME) );
 
 		$contenttypes = array("json"=>"application/json","csv"=>"text/csv", "tsv"=>"text/tsv","ttl"=>"text/turtle");
 
@@ -19,22 +20,22 @@ class org {
 			case "csv":
 			case "tsv":
 			case "ttl":
-				if(!file_exists("data/org/$idtype-$id.$suffix")){
+				if(!file_exists("data/org/$idtype-$id-$datset.$suffix")){
 					$f3->error(404);
 				}
 				header('Content-Type: '.$contenttypes[$suffix]);
-				print file_get_contents( "data/org/$idtype-$id.$suffix" );
+				print file_get_contents( "data/org/$idtype-$id-$datset.$suffix" );
 			break;
 			case "":
 			case "html":
-				if(!file_exists("data/org/$idtype-$id.json")){
+				if(!file_exists("data/org/$idtype-$id-$datset.json")){
 					$f3->error(404);
 				}
-		
-				$data = json_decode( file_get_contents( "data/org/$idtype-$id.json" ), true );
+				$data = json_decode( file_get_contents( "data/org/$idtype-$id-$datset.json" ), true );
 				$fields = json_decode( file_get_contents( "../var/uniquip-fields.json" ), true );
-
-				$f3->set('metadata', $data["metadata"] );
+				$eq = $f3->eq;
+				$metadata = $eq->get_dataset($datset, 'data_hash', array('crawl','org'));
+				$f3->set('metadata', $metadata );
 				$f3->set('records', $data["records"] );
 				$f3->set('fields', $fields );
 				$f3->set('html_title', "Equipment from ".$data["metadata"]["org_name"] );
