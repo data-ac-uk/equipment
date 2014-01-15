d6b1495372e8aeddf4f3dc79b9902ab5aea36cb8
Christopher Gutteridge <cjg@ecs.soton.ac.uk>
2014-01-15 15:10:10 +0000

    Fix config bug

---
+++ etc/eq_config.local.php
@@ -0,0 +1,11 @@
+<?php
+
+date_default_timezone_set('Europe/London');
+
+$eq_config->db->connection = "mysql:host=mysql3.ecs.soton.ac.uk;port=3306;dbname=equipmentdev";
+$eq_config->db->user = 'equipmentdev';
+$eq_config->db->password = 'tedi69weka';
+
+$eq_config->crawler->emailto = array("a.j.milsted@soton.ac.uk");
+
+
