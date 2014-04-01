0db8f1131d534c655e7384e56fc0080094b77ed8
Andrew Milsted <a.j.milsted@soton.ac.uk>
2014-04-01 11:23:06 +0100

    Added troubleshooting link

--- htdocs/app/status.php
+++ htdocs/app/status.php
@@ -143,6 +143,8 @@ END;
 		#$c []= "<pre>".htmlspecialchars( print_r($status ,true))."</pre>";
 
 
+		$c []="If your data is not listed here and you expect it to be, check out our <a href=\"/troubleshooting\" title=\"Troubleshooting\">Troubleshooting page</a>";
+		
 		$c []= "<h3>Totals</h3>";
 		$c []= "This archive contains ".number_format($status['totals']['items'],0)." items from {$status['totals']['orgs']} organisations.";
 
