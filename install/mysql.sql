376421900f2d7af6a5ed8b6c4e97a40ee9abcf6f
Andrew Milsted <a.j.milsted@soton.ac.uk>
2015-02-27 10:25:25 +0000

    Updated sql

--- install/mysql.sql
+++ install/mysql.sql
@@ -1,28 +1,26 @@
--- MySQL dump 10.13  Distrib 5.5.31, for debian-linux-gnu (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 3.4.11.1deb2+deb7u1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: equipment
--- ------------------------------------------------------
--- Server version	5.5.31-0+wheezy1
+-- Host: localhost
+-- Generation Time: Feb 27, 2015 at 10:22 AM
+-- Server version: 5.5.40
+-- PHP Version: 5.4.35-0+deb7u2
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `equipment`
+--
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `autoOPDs`
 --
 
-DROP TABLE IF EXISTS `autoOPDs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `autoOPDs` (
+CREATE TABLE IF NOT EXISTS `autoOPDs` (
   `opd_id` varchar(255) NOT NULL,
   `opd_url` varchar(255) NOT NULL,
   `opd_ena` tinyint(1) NOT NULL,
@@ -33,16 +31,14 @@ CREATE TABLE `autoOPDs` (
   `opd_src` varchar(20) NOT NULL,
   PRIMARY KEY (`opd_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `crawls`
 --
 
-DROP TABLE IF EXISTS `crawls`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `crawls` (
+CREATE TABLE IF NOT EXISTS `crawls` (
   `crawl_id` int(11) NOT NULL AUTO_INCREMENT,
   `crawl_dataset` varchar(255) NOT NULL,
   `crawl_timestamp` datetime NOT NULL,
@@ -52,17 +48,15 @@ CREATE TABLE `crawls` (
   `crawl_gong` varchar(12) NOT NULL,
   `crawl_gong_json` text NOT NULL,
   PRIMARY KEY (`crawl_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=99 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `datasets`
 --
 
-DROP TABLE IF EXISTS `datasets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `datasets` (
+CREATE TABLE IF NOT EXISTS `datasets` (
   `data_uri` varchar(255) NOT NULL,
   `data_org` varchar(255) NOT NULL,
   `data_conforms` varchar(255) NOT NULL,
@@ -78,16 +72,41 @@ CREATE TABLE `datasets` (
   `data_src` varchar(32) NOT NULL,
   PRIMARY KEY (`data_uri`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groupLinks`
+--
+
+CREATE TABLE IF NOT EXISTS `groupLinks` (
+  `link_group` varchar(255) NOT NULL,
+  `link_org` varchar(255) NOT NULL,
+  PRIMARY KEY (`link_group`,`link_org`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `groups`
+--
+
+CREATE TABLE IF NOT EXISTS `groups` (
+  `group_id` varchar(255) NOT NULL,
+  `group_type` varchar(127) NOT NULL,
+  `group_name` varchar(255) NOT NULL,
+  `group_sname` varchar(255) NOT NULL,
+  PRIMARY KEY (`group_id`),
+  KEY `group_type` (`group_type`,`group_sname`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `itemPages`
 --
 
-DROP TABLE IF EXISTS `itemPages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `itemPages` (
+CREATE TABLE IF NOT EXISTS `itemPages` (
   `page_id` varchar(64) NOT NULL,
   `page_org` varchar(255) NOT NULL,
   `page_dataset` varchar(255) NOT NULL,
@@ -96,16 +115,14 @@ CREATE TABLE `itemPages` (
   `page_updated` datetime NOT NULL,
   PRIMARY KEY (`page_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `itemRDF`
 --
 
-DROP TABLE IF EXISTS `itemRDF`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `itemRDF` (
+CREATE TABLE IF NOT EXISTS `itemRDF` (
   `rdf_id` varchar(64) NOT NULL,
   `rdf_org` varchar(255) NOT NULL,
   `rdf_dataset` varchar(255) NOT NULL,
@@ -113,16 +130,29 @@ CREATE TABLE `itemRDF` (
   `rdf_updated` datetime NOT NULL,
   PRIMARY KEY (`rdf_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `items`
+--
+
+CREATE TABLE IF NOT EXISTS `items` (
+  `item_id` varchar(64) NOT NULL,
+  `item_org` varchar(255) NOT NULL,
+  `item_dataset` varchar(255) NOT NULL,
+  `item_location` varchar(255) NOT NULL,
+  `item_updated` datetime NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `itemUniquips`
 --
 
-DROP TABLE IF EXISTS `itemUniquips`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `itemUniquips` (
+CREATE TABLE IF NOT EXISTS `itemUniquips` (
   `itemU_id` varchar(64) NOT NULL,
   `itemU_org` varchar(255) NOT NULL,
   `itemU_dataset` varchar(255) NOT NULL,
@@ -153,33 +183,14 @@ CREATE TABLE `itemUniquips` (
   KEY `itemU_dataset` (`itemU_dataset`),
   FULLTEXT KEY `textsearch` (`itemU_f_name`,`itemU_f_desc`,`itemU_f_technique`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `items`
---
-
-DROP TABLE IF EXISTS `items`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `items` (
-  `item_id` varchar(64) NOT NULL,
-  `item_org` varchar(255) NOT NULL,
-  `item_dataset` varchar(255) NOT NULL,
-  `item_location` varchar(255) NOT NULL,
-  `item_updated` datetime NOT NULL,
-  PRIMARY KEY (`item_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `locations`
 --
 
-DROP TABLE IF EXISTS `locations`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `locations` (
+CREATE TABLE IF NOT EXISTS `locations` (
   `loc_uri` varchar(255) NOT NULL,
   `loc_point` point NOT NULL,
   `loc_lat` float NOT NULL,
@@ -189,16 +200,32 @@ CREATE TABLE `locations` (
   `loc_updated` datetime NOT NULL,
   PRIMARY KEY (`loc_uri`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `message_id` int(11) NOT NULL AUTO_INCREMENT,
+  `message_to` varchar(255) NOT NULL,
+  `message_subject` varchar(255) NOT NULL,
+  `message_body` text NOT NULL,
+  `message_type` varchar(20) NOT NULL,
+  `message_link` varchar(255) NOT NULL,
+  `message_time` datetime NOT NULL,
+  `message_sent` tinyint(1) NOT NULL,
+  PRIMARY KEY (`message_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
 
 --
 -- Table structure for table `orgs`
 --
 
-DROP TABLE IF EXISTS `orgs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `orgs` (
+CREATE TABLE IF NOT EXISTS `orgs` (
   `org_uri` varchar(255) NOT NULL,
   `org_idscheme` varchar(20) NOT NULL,
   `org_id` varchar(64) NOT NULL,
@@ -212,15 +239,52 @@ CREATE TABLE `orgs` (
   `org_lastseen` datetime NOT NULL,
   PRIMARY KEY (`org_uri`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- --------------------------------------------------------
 
--- Dump completed on 2014-01-07  9:58:55
+--
+-- Table structure for table `orgsManual`
+--
+
+CREATE TABLE IF NOT EXISTS `orgsManual` (
+  `org_xid` varchar(255) NOT NULL,
+  `dataset_type` varchar(32) NOT NULL,
+  `dataset_url` varchar(255) NOT NULL,
+  `org_name` varchar(255) NOT NULL,
+  `org_url` varchar(255) NOT NULL,
+  `org_logo` varchar(255) NOT NULL,
+  `dataset_corrections` varchar(255) NOT NULL,
+  `dataset_contact` varchar(255) NOT NULL,
+  `org_enabled` tinyint(1) NOT NULL,
+  PRIMARY KEY (`org_xid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statsIPOwner`
+--
+
+CREATE TABLE IF NOT EXISTS `statsIPOwner` (
+  `ip_address` varchar(32) NOT NULL,
+  `ip_owner` varchar(255) NOT NULL,
+  `ip_date` datetime NOT NULL,
+  PRIMARY KEY (`ip_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `statsSearchTerms`
+--
+
+CREATE TABLE IF NOT EXISTS `statsSearchTerms` (
+  `search_key` varchar(127) NOT NULL,
+  `search_ip` varchar(16) NOT NULL,
+  `search_date` datetime NOT NULL,
+  `search_term` varchar(255) NOT NULL,
+  `search_owner` varchar(255) NOT NULL,
+  PRIMARY KEY (`search_key`),
+  KEY `search_term` (`search_term`),
+  KEY `search_owner` (`search_owner`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
