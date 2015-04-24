188865eef76586e7f819580fffa6c24867a5c804
Andrew Milsted <a.j.milsted@soton.ac.uk>
2015-04-24 15:29:59 +0100

    sql testing

--- htdocs/app/api.php
+++ htdocs/app/api.php
@@ -101,6 +101,12 @@ class api {
 		$sql_where = "WHERE ".join(" AND ",$sql_where);
 		$count = $eq->db->exec("SELECT count(`item_id`) as tcount $sql_sel  {$sql_from} {$sql_where}", $sql_params);
 		
+		if($_REQUEST['showsql']){
+			echo "<pre>";
+			echo "SELECT count(`item_id`) as tcount $sql_sel  {$sql_from} {$sql_where}\n";
+			print_r($sql_params);
+		}
+		
 		if(isset($_REQUEST['page'])){
 			if( (int)$_REQUEST['page'] >= ceil($count[0]['tcount']/$params['page_size'])){
 				die("Page no too high!");
